ce4e45f1b6ce0eb9d765ab23b2d2d468168a9b8e
Maria Grigorova <mmgrigorova@gmail.com>
2018-04-04 20:14:02 +0300

    Solution of Subset of Sum

--- src/com/mmgrigorova/Arrays/SubsetofSumSArrayList.java
+++ src/com/mmgrigorova/Arrays/SubsetofSumSArrayList.java
@@ -4,7 +4,6 @@ import java.io.ByteArrayInputStream;
 import java.util.*;
 
 /**
- * IN PROGRESS...
  * Subset of Sum S
  * http://judge.telerikacademy.com/problem/16subsetsums
  * We are given an array of integers and a number S. Write a program to find if there exists a subset of the elements
@@ -13,10 +12,16 @@ import java.util.*;
 
 public class SubsetofSumSArrayList {
     static void fakeInput() {
-        String test = "2 1 2 4 3 5 2 6\n" +
-        "14";
+//        String test = "14\n" +
+//                "2 1 2 4 3 5 2 6";
 //        String test = "10\n" +
 //                "1 1 1 1 1 1 1 1 1 11";
+//        String test = "27\n" +
+//                "18 11";
+//        String test = "25\n" +
+//                "19 4 21";
+        String test = "26\n" +
+                "29 12 11 4 18 2 1 13 15";
         System.setIn(new ByteArrayInputStream(test.getBytes()));
     }
 
@@ -24,26 +29,29 @@ public class SubsetofSumSArrayList {
        fakeInput();
 
         Scanner in = new Scanner(System.in);
-        String[] arrayStrings = in.nextLine().split(" ");
         int s = in.nextInt();
+        in.nextLine();
+        String[] arrayStrings = in.nextLine().split(" ");
+
 
         ArrayList<Integer> array = new ArrayList<>();
-        int[] subset = new int[array.size()];
 
         for (String num : arrayStrings) {
             int value = Integer.parseInt(num);
             array.add(value);
         }
 
-        Collections.sort(array);
-        int sum = 0;
+        Collections.sort(array, Collections.reverseOrder());
+        Collections.reverse(array);
+        boolean isSubset = false;
 
-        for (int subsetStart = 0; subsetStart < array.size(); subsetStart++) {
-            for (int currentNumber = 0; currentNumber < array.size(); currentNumber++) {
-                if (sum +  array.get(currentNumber)<= s) {
-                    sum = sum + array.get(currentNumber);
+        for (int subsetStart = 0; subsetStart < array.size() - 1; subsetStart++) {
+            int sum = array.get(subsetStart);
+            for (int nextNumber = subsetStart+1; nextNumber < array.size(); nextNumber++) {
+                if (sum +  array.get(nextNumber)<= s) {
+                    sum = sum + array.get(nextNumber);
                     if (sum == s){
-                        System.out.println("yes");
+                        isSubset = true;
                         break;
                     }
                 } else {
@@ -52,9 +60,7 @@ public class SubsetofSumSArrayList {
             }
         }
 
-        if (sum==0){
-            System.out.println("no");
-        }
+        System.out.println(isSubset ? "yes" :"no");
 
     }
 
