82e308c91a4552cfe8ae0106af3ec0abcf24f0e5
Maria Grigorova <mmgrigorova@gmail.com>
2018-06-08 18:11:21 +0300

    Supermarket with InputReader

--- DS_AL_DP/src/exam2/SupermarketQueue.java
+++ DS_AL_DP/src/exam2/SupermarketQueue.java
@@ -1,21 +1,22 @@
 package exam2;
 
-import java.io.ByteArrayInputStream;
+import java.io.*;
 import java.util.*;
 
 public class SupermarketQueue {
     private static final String OKAY = "OK\n";
     private static final String ERROR = "Error\n";
-    private static Queue<String> queue = new LinkedList<>();
+    private static LinkedList<String> queue = new LinkedList<>();
     private static Map<String, Integer> peopleCountByName = new HashMap<>();
     private static StringBuilder result = new StringBuilder();
 
     public static void main(String[] args) {
         fakeInput();
-        Scanner in = new Scanner(System.in);
+        InputReader in = new InputReader();
+        OutputWriter out = new OutputWriter();
 
         while (true) {
-            String command[] = in.nextLine().split(" ");
+            String command[] = in.readLine().split(" ");
             if (command[0].equals("End")) {
                 break;
             }
@@ -40,7 +41,8 @@ public class SupermarketQueue {
             }
         }
 
-        System.out.println(result);
+        out.printLine(result);
+        out.close();
 
     }
 
@@ -55,8 +57,6 @@ public class SupermarketQueue {
             result.append(served);
             result.append(" ");
             peopleCountByName.merge(served, -1, Integer::sum);
-
-
         }
         result.append("\n");
     }
@@ -76,8 +76,7 @@ public class SupermarketQueue {
             result.append(ERROR);
             return;
         }
-        LinkedList<String> queueAsList = (LinkedList<String>) queue;
-        queueAsList.add(position, person);
+        queue.add(position, person);
         peopleCountByName.merge(person, 1, Integer::sum);
         result.append(OKAY);
     }
@@ -110,5 +109,157 @@ public class SupermarketQueue {
                 "Insert 1 Ina\n" +
                 "End";
         System.setIn(new ByteArrayInputStream(test.getBytes()));
+    }
+
+    static class InputReader {
+        private InputStream stream;
+        private byte[] buf = new byte[1024];
+        private int curChar;
+        private int numChars;
+
+        InputReader() {
+            this.stream = System.in;
+        }
+
+        int read() {
+            if (numChars == -1)
+                throw new InputMismatchException();
+            if (curChar >= numChars) {
+                curChar = 0;
+                try {
+                    numChars = stream.read(buf);
+                } catch (IOException e) {
+                    throw new InputMismatchException();
+                }
+                if (numChars <= 0)
+                    return -1;
+            }
+            return buf[curChar++];
+        }
+
+        int readInt() {
+            int c = read();
+            while (isSpaceChar(c)) {
+                c = read();
+            }
+            int sgn = 1;
+            if (c == '-') {
+                sgn = -1;
+                c = read();
+            }
+            int res = 0;
+            do {
+                if (c < '0' || c > '9') {
+                    throw new InputMismatchException();
+                }
+                res *= 10;
+                res += c - '0';
+                c = read();
+            } while (!isSpaceChar(c));
+            return res * sgn;
+        }
+
+        long readLong() {
+            int c = read();
+            while (isSpaceChar(c)) {
+                c = read();
+            }
+            int sgn = 1;
+            if (c == '-') {
+                sgn = -1;
+                c = read();
+            }
+            long res = 0;
+            do {
+                if (c < '0' || c > '9') {
+                    throw new InputMismatchException();
+                }
+                res *= 10;
+                res += c - '0';
+                c = read();
+            } while (!isSpaceChar(c));
+            return res * sgn;
+        }
+
+        double readDouble() {
+            int c = read();
+            while (isSpaceChar(c)) {
+                c = read();
+            }
+            int sgn = 1;
+            if (c == '-') {
+                sgn = -1;
+                c = read();
+            }
+            double res = 0;
+            while (!isSpaceChar(c) && c != '.' && c != ',') {
+                if (c == 'e' || c == 'E') {
+                    return res * Math.pow(10, readInt());
+                }
+                if (c < '0' || c > '9') {
+                    throw new InputMismatchException();
+                }
+                res *= 10;
+                res += c - '0';
+                c = read();
+            }
+            if (c == '.' || c == ',') {
+                c = read();
+                double m = 1;
+                while (!isSpaceChar(c)) {
+                    if (c == 'e' || c == 'E') {
+                        return res * Math.pow(10, readInt());
+                    }
+                    if (c < '0' || c > '9') {
+                        throw new InputMismatchException();
+                    }
+                    m /= 10;
+                    res += (c - '0') * m;
+                    c = read();
+                }
+            }
+            return res * sgn;
+        }
+
+        String readLine() {
+            int c = read();
+            while (isSpaceChar(c))
+                c = read();
+            StringBuilder res = new StringBuilder();
+            do {
+                res.appendCodePoint(c);
+                c = read();
+            } while (!isSpaceChar(c));
+            return res.toString();
+        }
+
+        boolean isSpaceChar(int c) {
+            return c == '\n' || c == '\r' || c == '\t' || c == -1;
+        }
+    }
+
+    static class OutputWriter {
+        private final PrintWriter writer;
+
+        OutputWriter() {
+            writer = new PrintWriter(new BufferedWriter(new OutputStreamWriter(System.out)));
+        }
+
+        void print(Object... objects) {
+            for (int i = 0; i < objects.length; i++) {
+                if (i != 0)
+                    writer.print(' ');
+                writer.print(objects[i]);
+            }
+        }
+
+        void printLine(Object... objects) {
+            print(objects);
+            writer.println();
+        }
+
+        void close() {
+            writer.close();
+        }
     }
 }
