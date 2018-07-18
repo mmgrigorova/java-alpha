68262e9b91ebabbe1b3f26a91f371eb3f840879f
Maria Grigorova <mmgrigorova@gmail.com>
2018-07-18 17:23:44 +0300

    Return employees result set

--- JDBCDemo/src/main/java/com/mgrigorova/springjdbcdemo/web/EmployeesRestController.java
+++ JDBCDemo/src/main/java/com/mgrigorova/springjdbcdemo/web/EmployeesRestController.java
@@ -1,23 +1,24 @@
 package com.mgrigorova.springjdbcdemo.web;
 
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.sql.*;
-import java.util.Properties;
+import java.io.*;
 
+import java.sql.*;
+import java.util.*;
+
+import com.mgrigorova.springjdbcdemo.models.Employee;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 
 @RestController
 @RequestMapping("/api/employees")
 public class EmployeesRestController {
 
     @RequestMapping("/")
-    void getAll() {
+    public List<Employee> getAll() {
         Properties dbConfig = new Properties();
 
-        String configFilePath = "src\\main\\resources\\application.properties";
+        String configFilePath = "/Users/Maria/repos/java-alpha/JDBCDemo/src/main/resources/application.properties";
 
         try (FileInputStream fis = new FileInputStream(configFilePath)) {
             dbConfig.load(fis);
@@ -32,12 +33,29 @@ public class EmployeesRestController {
         String dbUser = dbConfig.getProperty("dbUser");
         String dbPass = dbConfig.getProperty("dbPass");
 
-        try (Connection connection = DriverManager.getConnection(dbUrl, dbUser, dbPass)) {
+        List<Employee> employeesList = new ArrayList<>();
+
+        try (
+                Connection connection = DriverManager.getConnection(dbUrl, dbUser, dbPass);
+                Statement statement = connection.createStatement();
+        ) {
             System.out.println("Connection created.");
+            String query = "select `FirstName`, `LastName`, `JobTitle` from employees;";
+            ResultSet result = statement.executeQuery(query);
+            while (result.next()) {
+                Employee e = new Employee(
+                        result.getString("FirstName"),
+                        result.getString("LastName"),
+                        result.getString("JobTitle")
+                );
+
+                employeesList.add(e);
+            }
         } catch (SQLException e) {
             e.printStackTrace();
             System.out.println("Connection failed :( .");
         }
 
+        return employeesList;
     }
 }
