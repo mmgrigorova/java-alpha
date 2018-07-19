b9cab6ac4ff2aa9b81a2803233ca7bc347cd78aa
Maria Grigorova <mmgrigorova@gmail.com>
2018-07-19 14:59:46 +0300

    Added hybernate demo project

---
+++ hybernatedemo/src/main/java/com/mmgrigorova/springhybernatedemo/SpringhybernatedemoApplication.java
@@ -0,0 +1,12 @@
+package com.mmgrigorova.springhybernatedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringhybernatedemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringhybernatedemoApplication.class, args);
+    }
+}
