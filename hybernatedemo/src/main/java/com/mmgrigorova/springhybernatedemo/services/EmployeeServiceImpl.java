76e7f7540124f98b30ef98ec02a392b8195be9c9
Maria Grigorova <mmgrigorova@gmail.com>
2018-07-20 15:18:19 +0300

    Create Employee Service

--- hybernatedemo/src/main/java/com/mmgrigorova/springhybernatedemo/services/EmployeeServiceImpl.java
+++ hybernatedemo/src/main/java/com/mmgrigorova/springhybernatedemo/services/EmployeeServiceImpl.java
@@ -1,5 +1,24 @@
 package com.mmgrigorova.springhybernatedemo.services;
 
-public class EmployeeServiceImpl {
+import com.mmgrigorova.springhybernatedemo.data.EmployeeRepository;
+import com.mmgrigorova.springhybernatedemo.models.Employee;
 
+import java.util.List;
+
+public class EmployeeServiceImpl implements EmployeeService{
+    private EmployeeRepository employeeRepository;
+
+    public EmployeeServiceImpl(EmployeeRepository employeeRepository) {
+        this.employeeRepository = employeeRepository;
+    }
+
+    @Override
+    public Employee getById(int id) {
+        return employeeRepository.getById(id);
+    }
+
+    @Override
+    public List<Employee> getAll() {
+        return employeeRepository.getAll();
+    }
 }
